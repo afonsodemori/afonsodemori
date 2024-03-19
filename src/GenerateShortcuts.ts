74e83b33cf40582f219b6e24025467c30a446a4e
Afonso de Mori <me@afonso.dev>
2024-03-19 06:50:57 +0100

    fix: Adapt to Node12 (cloudflare s version)

--- src/GenerateShortcuts.ts
+++ src/GenerateShortcuts.ts
@@ -1,43 +1,46 @@
 import Shortcut from "./Model/Shortcut";
 import * as fs from "fs";
-
+import axios from "axios";
 // TODO: Check this whole flow and refactor
 export default class GenerateShortcuts {
     feed = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vRny4h8y8u_z3FDv3JaLt7PZuB0zy1VzX4ep7E9gD-tihrZGpeNT6AUS33B8FM5xpN22WRz5qeQaQUs/pub?gid=2039462908&single=true&output=csv';
     outputPath = 'dist/_redirects';
 
-    execute() {
+    async execute() {
         fs.truncate(this.outputPath, 0, err => {
             if (err) {
                 console.error('Failed to truncate file:', err);
             }
         })
 
-        fetch(this.feed)
-            .then(response => response.text())
-            .then(text => {
-                const rows = text.split('\n');
-                rows.forEach(row => {
-                    const shortcut = Shortcut.fromCsvRow(row);
-                    console.log('Generating static files for', shortcut);
-                    shortcut.names.forEach(name => {
-                        if (name === 'code' && shortcut.url === 'locatio') {
-                            // first line = header. TODO: improve this
-                            return;
-                        }
+        let text;
+        try {
+            const response = await axios.get(this.feed);
+            text = response.data;
+        } catch (e) {
+        }
 
-                        const outputLine = "/" + name + " " + shortcut.url + " 301\n";
-                        try {
-                            fs.appendFile(this.outputPath, outputLine, err => {
-                                if (err) {
-                                    console.error('Failed to append to file:', err);
-                                }
-                            });
-                        } catch (e) {
-                            console.log(e);
+        const rows = text.split('\n');
+        rows.forEach((row: any) => {
+            const shortcut = Shortcut.fromCsvRow(row);
+            console.log('Generating static files for', shortcut);
+            shortcut.names.forEach(name => {
+                if (name === 'code' && shortcut.url === 'locatio') {
+                    // first line = header. TODO: improve this
+                    return;
+                }
+
+                const outputLine = "/" + name + " " + shortcut.url + " 301\n";
+                try {
+                    fs.appendFile(this.outputPath, outputLine, err => {
+                        if (err) {
+                            console.error('Failed to append to file:', err);
                         }
                     });
-                });
+                } catch (e) {
+                    console.log(e);
+                }
             });
+        });
     }
 }
--- src/build.ts
+++ src/build.ts
@@ -1,3 +1,6 @@
 import GenerateShortcuts from "./GenerateShortcuts";
 
-new GenerateShortcuts().execute();
+new GenerateShortcuts().execute().then(r => {
+}).catch(error => {
+    console.error('Error executing GenerateShortcuts:', error);
+});
