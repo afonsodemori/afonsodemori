3cc39beaea7798051686b1cdd527e2eb38435e82
Afonso de Mori <me@afonso.dev>
2024-03-22 00:00:25 +0100

    feat: Replace php router with basic express server

---
+++ src/server.ts
@@ -0,0 +1,30 @@
+import express from 'express';
+import fs from 'fs';
+import path from 'path';
+
+const app = express();
+const BASE_DIR = path.join(__dirname, '../dist');
+
+app.use(express.static(BASE_DIR));
+
+app.get('*', (req, res) => {
+    const route = req.path;
+    const tryPaths = [
+        path.join(BASE_DIR, route),
+        path.join(BASE_DIR, `${route}.html`),
+        path.join(BASE_DIR, route, 'index.html'),
+    ];
+
+    for (const tryPath of tryPaths) {
+        if (fs.existsSync(tryPath)) {
+            res.sendFile(tryPath);
+            return;
+        }
+    }
+
+    res.status(404).send('Not Found');
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
