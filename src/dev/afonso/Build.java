5427a49cb194be88b1b74d9dc8c6c76471f6d161
Afonso de Mori <me@afonso.dev>
2024-03-21 19:45:22 +0100

    chore: Clean up old build files

--- src/dev/afonso/Build.java
+++ src/dev/afonso/Build.java
@@ -6,7 +6,5 @@ public class Build {
 
     public static void main(String[] args) throws IOException {
         PageGenerator.run();
-        // ResumesImporter.run();
-        // ShortcutsGenerator.run(); TODO: Moved to TS
     }
 }
