077e2eb6bcdb49a49b22c8e628769ad3b51e92d8
Afonso de Mori <me@afonso.dev>
2024-03-23 17:35:21 +0100

    feat: Add selects to CV page; link home to CV and remove curtain

--- resources/assets/scripts.js
+++ resources/assets/scripts.js
@@ -1,57 +1,17 @@
-/*
-const languageAnchors = document.querySelectorAll('.language a');
-const headers = document.querySelectorAll('header h2');
-const paragraphs = document.querySelectorAll('article p');
-const resumeDownloadLinks = document.querySelectorAll('footer .download-resume');
+const selectElements = document.querySelectorAll('select.download-cv');
+selectElements.forEach(select => {
+    select.addEventListener('change', event => {
+        const [language, format] = event.target.value.split('-');
+        const fileUrl = `/docs/cv-${language}-afonso_de_mori.${format}`;
 
-languageAnchors.forEach(anchor => {
-    anchor.addEventListener('click', (event) => {
-        event.preventDefault();
+        const tempLink = document.createElement('a');
+        tempLink.href = fileUrl;
+        tempLink.download = '';
 
-        const clickedLanguage = anchor.classList[0];
+        document.body.appendChild(tempLink);
+        tempLink.click();
+        document.body.removeChild(tempLink);
 
-        languageAnchors.forEach(languageAnchor => {
-            languageAnchor.classList.remove('hidden');
-        });
-        anchor.classList.add('hidden');
-
-        headers.forEach(header => {
-            header.classList.add('hidden');
-        });
-        document.querySelector(`header h2.${clickedLanguage}`).classList.remove('hidden');
-
-        paragraphs.forEach(paragraph => {
-            paragraph.classList.add('hidden');
-        });
-        document.querySelector(`article p.${clickedLanguage}`).classList.remove('hidden');
-
-        resumeDownloadLinks.forEach(link => {
-            link.classList.add('hidden');
-        });
-        document.querySelector(`footer a.${clickedLanguage}`).classList.remove('hidden');
+        event.target.value = '';
     });
-});
-*/
-
-const curtain = document.querySelector('#curtain');
-const downloadLinks = document.querySelectorAll('footer .download-resume');
-
-downloadLinks.forEach(link => {
-    link.addEventListener('click', (event) => {
-        event.preventDefault();
-        curtain.classList.remove('hidden');
-        document.body.style.overflow = 'hidden';
-    });
-});
-
-curtain.addEventListener('click', () => {
-    curtain.classList.add('hidden');
-    document.body.style.overflow = 'auto';
-});
-
-const resume = document.querySelector('.download-resume');
-resume.addEventListener('click', (e) => {
-    e.preventDefault()
-    curtain.style.display = 'flex';
-    document.body.style.overflow = 'hidden';
 });
