4d49e37fb9aa2b0928571bd71903293a6b12b333
Afonso de Mori <me@afonso.dev>
2024-05-15 23:40:36 +0200

    feat: Use low quality images as placeholder in CV page

--- bin/convert-cv.sh
+++ bin/convert-cv.sh
@@ -7,6 +7,7 @@ for pdf_file in "$pdf_dir"/cv-*.pdf; do
     filename_no_ext="${filename%.*}"
 
     pdftoppm "$pdf_file" "$pdf_dir/$filename_no_ext" -png
+    pdftoppm -r 10 "$pdf_file" "$pdf_dir/$filename_no_ext-low" -png
 
     for png_file in "$pdf_dir"/cv-*.png; do
         webp_file="$pdf_dir/$(basename -- "$png_file" .png).webp"
