34aedd07d08b928f524033a9233cfc0cdb1d19b0
Afonso de Mori <me@afonso.dev>
2024-05-16 18:26:31 +0200

    refactor: Minor refactoring

--- resources/assets/js/scripts.js
+++ resources/assets/js/scripts.js
@@ -27,26 +27,29 @@ function replaceLowQualityResume() {
     });
 }
 
+function eventsToDownloadResume() {
+    const selectElements = document.querySelectorAll('select.download-cv');
+    selectElements.forEach(select => {
+        select.addEventListener('change', event => {
+            const [language, format] = event.target.value.split('-');
+            const fileUrl = `/docs/cv-${language}-afonso_de_mori.${format}`;
+
+            const tempLink = document.createElement('a');
+            tempLink.href = fileUrl;
+            tempLink.download = '';
+
+            document.body.appendChild(tempLink);
+            tempLink.click();
+            document.body.removeChild(tempLink);
+
+            event.target.value = '';
+        });
+    });
+}
+
 window.addEventListener('load', () => {
     updateDefaultLocale();
     registerServiceWorker();
     replaceLowQualityResume();
-});
-
-const selectElements = document.querySelectorAll('select.download-cv');
-selectElements.forEach(select => {
-    select.addEventListener('change', event => {
-        const [language, format] = event.target.value.split('-');
-        const fileUrl = `/docs/cv-${language}-afonso_de_mori.${format}`;
-
-        const tempLink = document.createElement('a');
-        tempLink.href = fileUrl;
-        tempLink.download = '';
-
-        document.body.appendChild(tempLink);
-        tempLink.click();
-        document.body.removeChild(tempLink);
-
-        event.target.value = '';
-    });
+    eventsToDownloadResume();
 });
