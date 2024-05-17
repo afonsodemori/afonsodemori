823d6777a15078076e8a0960c8cc36773c3fd5be
Afonso de Mori <me@afonso.dev>
2024-05-17 23:12:23 +0200

    feat: Transform list to selects

--- resources/assets/js/scripts.js
+++ resources/assets/js/scripts.js
@@ -31,8 +31,7 @@ function eventsToDownloadResume() {
     const selectElements = document.querySelectorAll('select.download-cv');
     selectElements.forEach(select => {
         select.addEventListener('change', event => {
-            const [language, format] = event.target.value.split('-');
-            const fileUrl = `/docs/cv-${language}-afonso_de_mori.${format}?download`;
+            const fileUrl = `${event.target.value}?download`;
 
             const tempLink = document.createElement('a');
             tempLink.href = fileUrl;
@@ -52,3 +51,49 @@ window.addEventListener('load', () => {
     replaceLowQualityResume();
     eventsToDownloadResume();
 });
+
+const div = document.querySelector('div.cv-select-download');
+
+if (div) {
+    let elements = [];
+
+    elements.push(div.children.item(0));
+
+    for (let ul = 1; ul <= 3; ul++) {
+        const selectText = div.children.item(ul).children.item(0).firstChild.nodeValue;
+        const options = div.children.item(ul).children.item(0).children.item(0).children;
+
+        const select = document.createElement('div');
+        select.classList.add('custom-select');
+
+        const span = document.createElement('span');
+        span.classList.add('selected-option');
+        if (ul === 1) span.classList.add('primary');
+        span.textContent = `${selectText} ▾`;
+        select.appendChild(span);
+
+        const trueSelect = document.createElement('select');
+        trueSelect.classList.add('download-cv');
+        trueSelect.ariaLabel = selectText;
+
+        const disabledOption = document.createElement('option');
+        disabledOption.selected = true;
+        disabledOption.disabled = true;
+        disabledOption.value = '';
+        disabledOption.text = options.item(0).firstChild.nodeValue;
+        trueSelect.appendChild(disabledOption);
+
+        for (let option = 1; option <= 3; option++) {
+            const option1 = options.item(option);
+            const optionNode = document.createElement('option');
+            optionNode.value = new URL(option1.firstChild.href).pathname;
+            optionNode.text = option1.firstChild.textContent;
+            trueSelect.appendChild(optionNode);
+        }
+
+        select.appendChild(trueSelect);
+        elements.push(select);
+    }
+
+    div.replaceWith(...elements);
+}
